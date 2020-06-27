35d6f51bb390f6336efa446d66b6d2bb96abb947
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-06-27 17:57:35 -0400

    added aliases to bash_profile

---
+++ install.sh
@@ -0,0 +1,11 @@
+echo "alias h='history'" >> ~/.bash_profile
+echo "alias gh='history|grep'" >> ~/.bash_profile
+echo "alias ll='ls -lhF'" >> ~/.bash_profile
+echo "alias la='ls -lhAF'" >> ~/.bash_profile
+echo "alias lal='ls -lhAF | less'" >> ~/.bash_profile
+echo "alias cd..='cd ..'" >> ~/.bash_profile
+echo "alias cd...='cd ../..'" >> ~/.bash_profile
+echo "alias cd....='cd ../../..'" >> ~/.bash_profile
+echo "alias cd.....='cd ../../../..'" >> ~/.bash_profile
+echo "alias cd......='cd ../../../../..'" >> ~/.bash_profile
+source ~/.bash_profile
