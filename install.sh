000d527ce217f53637ba023043016eb78fe92f3f
Luke Starnes <luke.starnes@gtri.gatech.edu>
2020-07-01 23:36:46 -0400

    updated with new approach

--- install.sh
+++ install.sh
@@ -1,32 +1,17 @@
-# echo "alias h='history'" >> ~/.bash_profile
-# echo "alias gh='history|grep'" >> ~/.bash_profile
-# echo "alias ll='ls -lhF'" >> ~/.bash_profile
-# echo "alias la='ls -lhAF'" >> ~/.bash_profile
-# echo "alias lal='ls -lhAF | less'" >> ~/.bash_profile
-# echo "alias cd..='cd ..'" >> ~/.bash_profile
-# echo "alias cd...='cd ../..'" >> ~/.bash_profile
-# echo "alias cd....='cd ../../..'" >> ~/.bash_profile
-# echo "alias cd.....='cd ../../../..'" >> ~/.bash_profile
-# echo "alias cd......='cd ../../../../..'" >> ~/.bash_profile
-# source ~/.bash_profile
-
 #!/bin/bash
 
 ########## Variables
 
 dir="$(pwd)"                    # dotfiles directory
 backupDir="$HOME/.dotfiles_backup/$(date)"            # old dotfiles backup directory
+tmp_fn='.temp'
 files="bashrc"    # list of files/folders to symlink in homedir
+append_files="bashrc"  # list of files that will have new content appended (not replaced)
 
 ##########
 
 # Show working directory
 echo "Using working current: $dir"
-echo
-
-# init and update Git submodules
-echo "Init and update Git Submodules"
-git submodule update --init --recursive
 echo
 
 # create dotfiles_old in homedir
@@ -43,15 +28,53 @@ echo
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks 
 for file in $files; do
-    echo "Moving any existing dotfiles from $HOME/.$file to $backupDir/.$file"
-    mv "$HOME/.$file" "$backupDir/$file"
-    if [ -e "$HOME/.$file" ]
+    new_file_path="$dir"
+    new_file_path+="/.${file}"
+    original_file_path="$HOME/.$file"
+    echo "30 $new_file_path, $original_file_path"
+    for file_a in $append_files; do
+        echo "32 $file_a"
+        if [ "$file_a" == "$file" ] 
+        then
+            append_match=1
+            echo "36 -- match"
+            echo "append_match $append_match"
+            break
+        fi
+    done
+    
+    if [ -e $original_file_path ]
     then
-        echo "Removing existing dotfile"
-        rm "$HOME/.$file"
+        echo "45 $original_file_path exists"
+        if [ "$append_match" == "1" ] 
+        then
+            # echo "48 - append_match is true, $original_file_path, $new_file_path, $tmp_fn"
+            # prepend
+            # echo "$original_file_path + $new_file_path > $new_file_path"
+            # echo "------- $original_file_path -------"
+            # echo "$(cat $original_file_path)"
+            # echo
+            # echo "------- $new_file_path -------"
+            # echo "$(cat $new_file_path)"
+            # echo
+            # cat "$original_file_path" "$new_file_path" > "$tmp_fn"
+            for f in "$original_file_path" "$new_file_path"; do
+                # echo "62 $f" 
+                (cat "${f}"; echo) >> $tmp_fn
+            done
+            mv "$tmp_fn" "$new_file_path"
+            rm -f "$tmp_fn"
+            # echo "------- $new_file_path -------"
+            # echo "$(cat $new_file_path)"
+            
+        fi
+        echo "Moving any existing dotfiles from $original_file_path to $backupDir/.$file"
+        mv "$original_file_path" "$backupDir/$file"
+        # echo "Removing existing dotfile"
+        # rm "$HOME/.$file"
     fi
     echo "Creating symlink to $file in home directory."
-    ln -s "$dir/$file" "$HOME/.$file"
+    ln -s "$new_file_path" "$original_file_path"
     echo
 done
 echo 
